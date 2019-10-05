fbc051c347cf9c36192d3fb33b4b504ad10bf555
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-10-05 18:33:28 -0400

    Css works

--- ASP_assignment_1/Startup.cs
+++ ASP_assignment_1/Startup.cs
@@ -21,11 +21,12 @@ namespace ASP_assignment_1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+         
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
     }
