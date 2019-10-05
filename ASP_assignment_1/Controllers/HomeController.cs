bd292f07452a0829b3954ab3dab15614af005a6e
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-10-05 15:17:16 -0400

    Add project files.

---
+++ ASP_assignment_1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_assignment_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("Home");
+
+
+        public ViewResult RecipeList() => View();
+        public ViewResult AddRecipe() => View();
+        public ViewResult ReviewRecipe() => View();
+        public ViewResult ViewRecipe() => View();
+
+
+    }
+}
