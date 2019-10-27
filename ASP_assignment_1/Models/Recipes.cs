06935a0166312d7288822729074af08166b71411
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-10-27 14:06:19 -0400

    sd

---
+++ ASP_assignment_1/Models/Recipes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_assignment_1.Models
+{
+    public class Recipes
+    {
+        public int RecipeID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+       
+
+
+
+    }
+}
