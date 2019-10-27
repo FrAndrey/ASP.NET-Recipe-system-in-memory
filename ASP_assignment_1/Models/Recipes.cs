c68ca081afd093e71a4e3449fd5b950b0e318500
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-10-27 19:27:06 -0400

    stuck

--- ASP_assignment_1/Models/Recipes.cs
+++ ASP_assignment_1/Models/Recipes.cs
@@ -12,9 +12,16 @@ namespace asgn2.Models
         public string Ingredients { get; set; }
 
         public string Description { get; set; }
-       
 
 
+        /*public Recipes(int RecID, string nam, string Ingred, string Descr)
+        {
+            RecipeID = RecID;
+            Name = nam;
+            Ingredients = Ingred;
+            Description = Descr;
+
+        } */
 
     }
 }
--- ASP_assignment_1/Models/Repository.cs
+++ ASP_assignment_1/Models/Repository.cs
@@ -7,18 +7,32 @@ namespace asgn2.Models
 {
     public class Repository
     {
+        private static IQueryable<Recipes> recipes;
 
-        public static IQueryable<Recipes> Recipes => new List<Recipes>
-        {
-            new Recipes {RecipeID =1, Name = "Salmon",Ingredients="Ssd", Description="Blabla"},
-            new Recipes {RecipeID =2, Name = "Chicken",Ingredients="Ssd", Description="Ulala"},
-            new Recipes {RecipeID =3, Name = "Duck",Ingredients="Ssd", Description="Mak"}
-        }.AsQueryable<Recipes>();
+
+          public static IQueryable<Recipes> Recipes => new List<Recipes>
+          {
+
+              new Recipes {RecipeID =1, Name = "Salmon",Ingredients="Ssd", Description="Blabla"},
+              new Recipes {RecipeID =2, Name = "Chicken",Ingredients="Ssd", Description="Ulala"},
+              new Recipes {RecipeID =3, Name = "Duck",Ingredients="Ssd", Description="Mak"}
+          }.AsQueryable<Recipes>(); 
+
+        
+
+
+
 
 
         public static void AddResponse(Recipes guestRecipe)
         {
-            Recipes.ToList().Add(guestRecipe);
+            recipes.ToList().Add(guestRecipe);
+            foreach (var b in Recipes)
+            {
+                Console.WriteLine();
+            }
+
+
         }
 
 
