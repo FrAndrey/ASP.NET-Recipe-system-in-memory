6a936ba90dd20f672cc4f380204c754e85dd51f9
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-10-27 14:17:04 -0400

    rename of solution

---
+++ ASP_assignment_1/Models/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_assignment_1.Models
+{
+    public class Repository
+    {
+        private static List<Recipes> recipes = new List<Recipes>();
+        public static IEnumerable<Recipes> Recipes
+        {
+            get
+            {
+                return recipes;
+;
+            }
+        }
+        public static void AddResponse(Recipes guestRecipe)
+        {
+            recipes.Add(guestRecipe);
+        }
+
+
+
+
+
+
+    }
+}
