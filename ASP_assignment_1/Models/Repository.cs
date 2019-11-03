5ca3bf789f628e1e2b90ec4396567d0375d88e36
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-11-02 20:34:32 -0400

    Done!

--- ASP_assignment_1/Models/Repository.cs
+++ ASP_assignment_1/Models/Repository.cs
@@ -12,8 +12,8 @@ namespace asgn2.Models
         private static List<Recipes> allRecipes = new List<Recipes>()
         {
               new Recipes {RecipeID=0, Name="Salmon",Ingredients="Salmon, rice, lemon",Description="Put fish in the oven for 30 minutes",imagePath="~/pictures/salmon.jpg"},
-              new Recipes {RecipeID=1, Name = "Chicken",Ingredients="Chicken, spices, marinade mix", Description="Bake",imagePath="~/pictures/chiken.png"},
-              new Recipes {RecipeID=2, Name = "Duck",Ingredients="Duck, grapefruit, potatoes", Description="Bake",imagePath="~/pictures/duck.jpg"}
+              new Recipes {RecipeID=1, Name = "Chicken",Ingredients="Chicken, spices, marinade mix", Description="Bake 45 mins",imagePath="~/pictures/chiken.png"},
+              new Recipes {RecipeID=2, Name = "Duck",Ingredients="Duck, grapefruit, potatoes", Description="Bake at least 1 hour",imagePath="~/pictures/duck.jpg"}
         };
 
 
