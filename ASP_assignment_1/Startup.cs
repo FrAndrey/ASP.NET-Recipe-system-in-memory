97e8853fef3658b416d256230065113d855dd12b
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-11-09 16:11:44 -0500

    configuring DB

--- ASP_assignment_1/Startup.cs
+++ ASP_assignment_1/Startup.cs
@@ -7,16 +7,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using asgn2.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace asgn2
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration conf)
+        {
+            Configuration = conf;
+        }
+
+
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IRecipeRepository,Repository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration["Data:RecipeSystemRecipes:ConnectionString"])
+
+            );
+
+            services.AddTransient<IRecipeRepository,EFRecipeRepository>();
             services.AddMvc();
         }
 
